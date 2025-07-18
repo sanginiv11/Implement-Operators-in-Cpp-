a791e88c8a550e4e338278e8a72471385607c892
Sangini Verma <sanginiv11@gmail.com>
2025-07-19 01:45:03 +0530

    Add files via upload

---
+++ calcstudentavgmarks.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int m1, m2, m3, m4, m5, total;
+    float avg;
+    cout<<"Enter subject-1 marks: ";
+    cin>>m1;
+    cout<<"Enter subject-2 marks: ";
+    cin>>m2;
+    cout<<"Enter subject-3 marks: ";
+    cin>>m3;
+    cout<<"Enter subject-4 marks: ";
+    cin>>m4;
+    cout<<"Enter subject-5 marks: ";
+    cin>>m5;
+
+    avg = (m1 +m2 +m3 +m4 +m5 )/5.0;
+    cout<<"Percentage: "<<avg<<"%"<<endl;
+    if(avg>=90){
+        cout<<"Grade: O";
+    }
+    else if(avg>=85&&avg<90){
+        cout<<"Grade: A+";
+    }
+    else if(avg>=75&&avg<85){
+        cout<<"Grade: A";
+    }
+    else if(avg>=65&&avg<75){
+        cout<<"Grade: B+";
+    }
+    else if(avg>=55&&avg<65){
+        cout<<"Grade: B";
+    }
+    else if(avg>=40&&avg<55){
+        cout<<"Grade: C";
+    }
+    else {
+        cout<<"Fail";
+    }
+
+    return 0;
+}
+
+
