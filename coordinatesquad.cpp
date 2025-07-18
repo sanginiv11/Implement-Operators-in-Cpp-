0e99e72c2dabf1789383796fa8e104a6d3f016e5
Sangini Verma <sanginiv11@gmail.com>
2025-07-19 02:17:28 +0530

    Add files via upload

---
+++ coordinatesquad.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Enter x coordinate: ";
+    cin >> x;
+    cout << "Enter y coordinate: ";
+    cin >> y;
+    if(x > 0 && y > 0) {
+        cout << "Point (" << x << ", " << y << ") is in Quadrant I." << endl;
+    } else if(x < 0 && y > 0) {
+        cout << "Point (" << x << ", " << y << ") is in Quadrant II." << endl;
+    } else if(x < 0 && y < 0) {
+        cout << "Point (" << x << ", " << y << ") is in Quadrant III." << endl;
+    } else if(x > 0 && y < 0) {
+        cout << "Point (" << x << ", " << y << ") is in Quadrant IV." << endl;
+    } else if(x == 0 && y == 0) {
+        cout << "Point is at the origin." << endl;
+    } else if(x == 0) {
+        cout << "Point lies on the Y axis." << endl;
+    } else if(y == 0) {
+        cout << "Point lies on the X axis." << endl;
+    }
+    return 0;
+}
